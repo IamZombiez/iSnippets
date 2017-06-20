870318502aa9e16ec3b886f9871ac8e54ad28b5f
IamZombiez <dereksiu216@gmail.com>
2017-06-20 11:31:01 -0700

    models

--- mysite/form/models.py
+++ mysite/form/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class SnippetForm(models.Model):
+    title = models.CharField(max_length=128)
+    language = models.CharField(max_length=32)
+    code = models.CharField(max_length=256)
+    descript = models.CharField(max_length=256)
+    added_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s by %s' % (self.title, self.language)
